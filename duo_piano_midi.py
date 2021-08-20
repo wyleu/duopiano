04156531a85c591d0272eae23621d49caffe484d
Wyleu <wyleus@gmail.com>
2021-08-20 16:08:19 +0100

    First commit

---
+++ duo_piano_midi.py
@@ -0,0 +1,95 @@
+"""
+Ruggedising the duo piano
+
+Wake Up:
+    detect duopiano connection
+    send midi message ( which?) to
+    Duo-piano
+    while port:
+        # Keep awake?
+        send keep_alive
+       
+
+"""
+
+DUO_PIANO_STRING = "GENERAL:GENERAL MIDI 1"
+
+import time
+from types import resolve_bases
+import rtmidi
+
+
+class DuoPiano:
+    def __init__(self):
+        self.midiout = rtmidi.MidiOut()
+        self.midiin = rtmidi.MidiIn()
+
+        self.portsin = self.aquire_in_ports()
+        self.portsout = self.aquire_out_ports()
+
+    def aquire_in_ports(self):
+        return self.midiout.get_ports()
+
+    def aquire_out_ports(self):
+        return self.midiout.get_ports()
+
+    def get_duo_out(self):
+        """Find the term GENERAL MIDI
+        and identify list index
+
+        """
+
+        res_dict = {}
+
+        for count, item in enumerate(self.midiout.get_ports()):
+            if DUO_PIANO_STRING in item:
+                return self.midiout.open_port(count)
+
+        return None
+
+    def get_duo_in(self):
+        """Find the term GENERAL MIDI
+        and return open port
+
+        """
+        res_dict = {}
+
+        for count, item in enumerate(self.midiin.get_ports()):
+            print(count, item)
+
+            if DUO_PIANO_STRING in item:
+                return self.midiin.open_port(count)
+
+        return None
+
+    def strike_note(self, channel, note, velocity, duration):
+
+        """1, 60, 10,.25)"""
+        with self.midiout:
+            note_on = [0x90, 60, 112]  # channel 1, middle C, velocity 112
+            note_off = [0x80, 60, 0]
+            self.midiout.send_message(note_on)
+            time.sleep(0.5)
+            self.midiout.send_message(note_off)
+            time.sleep(0.1)
+
+        del self.midiout
+
+
+midiout = rtmidi.MidiOut()
+available_ports = midiout.get_ports()
+
+if available_ports:
+    midiout.open_port(0)
+else:
+    midiout.open_virtual_port("My virtual output")
+
+with midiout:
+    note_on = [0x90, 60, 112]  # channel 1, middle C, velocity 112
+    note_off = [0x80, 60, 0]
+    midiout.send_message(note_on)
+    time.sleep(0.5)
+    midiout.send_message(note_off)
+    time.sleep(0.1)
+
+del midiout
---
+++ test_duo_piano_midi.py
@@ -0,0 +1,94 @@
+"""
+test duo_piano_midi.py
+"""
+
+import unittest
+from duo_piano_midi import DuoPiano
+from keyboard_wait import keyread
+
+
+# class TestKeyboardWait(unittest.TestCase):
+#     def test_user_for_P_key_yn_false(self):
+#         key = keyread("Press the P Key: And Press Enter...", yn=False)
+#         self.assertEqual(key, "P")
+
+#     def test_user_for_P_key_yn_false(self):
+#         key = keyread("Press the P Key: And Press Enter...")
+#         self.assertEqual(key, False)
+
+#     def test_timeout(self):
+#         key = keyread("Dont press anything for 5 seconds")
+#         self.assertIsNone(key)
+
+
+class TestDuoPianoMidi(unittest.TestCase):
+
+    name = "GENERAL:GENERAL MIDI 1 28:0"
+
+    def test_duo_piano_out_port_aquire(self):
+        duo_piano = DuoPiano()
+        self.assertEqual(duo_piano.portsout, duo_piano.aquire_out_ports())
+
+    def test_duo_piano_in_port_aquire(self):
+        duo_piano = DuoPiano()
+        self.assertEqual(duo_piano.portsin, duo_piano.aquire_in_ports())
+
+    def test_duo_piano_in_port_recognised_portsin(self):
+        duo_piano = DuoPiano()
+
+        self.assertIn(self.name, duo_piano.portsin)
+
+    def test_duo_piano_out_port_recognised_portsout(self):
+        duo_piano = DuoPiano()
+
+        self.assertIn(self.name, duo_piano.portsout)
+
+    def test_duo_piano_in_port_recognised_aquire_in(self):
+        duo_piano = DuoPiano()
+
+        self.assertIn(self.name, duo_piano.portsin)
+
+    def test_duo_piano_out_port_recognised_aquire_out(self):
+        duo_piano = DuoPiano()
+
+        self.assertIn(self.name, duo_piano.portsout)
+
+    def test_recognised_in_port_and_open(self):
+        """
+        Find one and only one (the first) DuoPiano IN MIDI port
+        """
+        duo_piano = DuoPiano()
+        din = duo_piano.get_duo_in()
+        self.assertIsInstance(din, type(duo_piano.midiin))
+        
+
+
+    def test_recognised_out_port_and_open(self):
+        """
+        Find one and only one (the first) DuoPiano OUT MIDI port
+        """
+        duo_piano = DuoPiano()
+        dout = duo_piano.get_duo_out()
+        self.assertIsInstance(dout, type(duo_piano.midiout))
+
+
+
+class TestDuoPianoMidiOut(unittest.TestCase):
+    # def test_middle_c_quiet_midi_channel_1(self):
+    #     duo_piano = DuoPiano()
+    #     duo_piano.strike_note(1, 60, 10, 0.25)
+
+    #     keyread("Did You hear that Quiet note:...? (yY/nN")
+
+    #     duo_piano.strike_note(1, 60, 100, 0.25)
+    #     keyread("Did You hear that Loud Note:...? (yY/nN")
+
+    def test_middle_c_loud_midi_channel_1(self):
+        duo_piano = DuoPiano()
+        dout = duo_piano.get_duo_out()
+        duo_piano.strike_note(1,60,10,.25)
+        print('DID YOU HEAR IT...?')
+
+
+if __name__ == "__main__":
+    unittest.main()
